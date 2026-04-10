619f6e07c1b517ff31577b467627a3c3adae3977
Jose Angel <joseangelmarquez857@gmail.com>
2026-04-09 20:28:30 -0600

    Configurar host 0.0.0.0 para Render

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = require('./src/app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+// Agregamos '0.0.0.0' como segundo parámetro aquí 👇
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
 });
